cbb195dd62d846959d22ec29715f2da75236cfa5
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2024-04-15 21:31:04 -0300

    creating page course

--- src/app/pages/components/banner/banner.component.ts
+++ src/app/pages/components/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -9,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class BannerComponent {
 
+  @Input()
+  banner_img = ""
+
+  @Input()
+  title = ""
+
+  @Input()
+  sub_title = ""
 }
--- src/app/pages/course/course.component.ts
+++ src/app/pages/course/course.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from "../components/banner/banner.component";
+import { NavBarComponent } from "../components/nav-bar/nav-bar.component";
+import { CursoComponent } from "../components/curso/curso.component";
+import { CardComponent } from "../components/card/card.component";
+import { LocationComponent } from "../components/location/location.component";
 
 @Component({
-  selector: 'app-course',
-  standalone: true,
-  imports: [],
-  templateUrl: './course.component.html',
-  styleUrl: './course.component.css'
+    selector: 'app-course',
+    standalone: true,
+    templateUrl: './course.component.html',
+    styleUrl: './course.component.css',
+    imports: [BannerComponent, NavBarComponent, CursoComponent, CardComponent, LocationComponent]
 })
 export class CourseComponent {
 

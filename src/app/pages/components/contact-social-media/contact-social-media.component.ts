aac243941585b6b87b0ca9e3563cba4369cd6bdf
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2024-04-25 22:11:16 -0300

    criando component social-media

---
+++ src/app/pages/components/contact-social-media/contact-social-media.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-social-media',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-social-media.component.html',
+  styleUrl: './contact-social-media.component.css'
+})
+export class ContactSocialMediaComponent {
+
+}
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -3,13 +3,14 @@ import { NavBarComponent } from "../components/nav-bar/nav-bar.component";
 import { BannerComponent } from "../components/banner/banner.component";
 import { FooterComponent } from "../components/footer/footer.component";
 import { ContactUsComponent } from "../components/contact-us/contact-us.component";
+import { ContactSocialMediaComponent } from "../components/contact-social-media/contact-social-media.component";
 
 @Component({
     selector: 'app-contact',
     standalone: true,
     templateUrl: './contact.component.html',
     styleUrl: './contact.component.css',
-    imports: [NavBarComponent, BannerComponent, FooterComponent, ContactUsComponent]
+    imports: [NavBarComponent, BannerComponent, FooterComponent, ContactUsComponent, ContactSocialMediaComponent]
 })
 export class ContactComponent {
 

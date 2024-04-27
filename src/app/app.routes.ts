ec0bec2572ada2b94f9b8532066c732be4d91ab1
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2024-04-27 14:35:37 -0300

    criando page-studio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CourseComponent } from './pages/course/course.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { PageStudioComponent } from './pages/page-studio/page-studio.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'cursos', component: CourseComponent},
   {path: 'contato', component: ContactComponent},
+  {path: 'studio', component: PageStudioComponent},
 ];
---
+++ src/app/pages/page-studio/page-studio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-studio',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-studio.component.html',
+  styleUrl: './page-studio.component.css'
+})
+export class PageStudioComponent {
+
+}

80c640010f2510ff7af513fbb4835df3e71d3532
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2024-04-30 22:25:32 -0300

    component procedures-sob

---
+++ src/app/pages/procedures-sob/procedures-sob.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-procedures-sob',
+  standalone: true,
+  imports: [],
+  templateUrl: './procedures-sob.component.html',
+  styleUrl: './procedures-sob.component.css'
+})
+export class ProceduresSobComponent {
+
+}

40348afcbfc3a936021a4b26bb83dfcc9bc410aa
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2024-05-10 23:29:31 -0300

    inseringo gallery

--- src/app/pages/procedures-lab/procedures-lab.component.ts
+++ src/app/pages/procedures-lab/procedures-lab.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from "../components/nav-bar/nav-bar.component";
 import { BannerComponent } from "../components/banner/banner.component";
 import { FooterComponent } from "../components/footer/footer.component";
+import { GalleryComponent } from "../components/gallery/gallery.component";
 
 @Component({
     selector: 'app-procedures-lab',
     standalone: true,
     templateUrl: './procedures-lab.component.html',
     styleUrl: './procedures-lab.component.css',
-    imports: [NavBarComponent, BannerComponent, FooterComponent]
+    imports: [NavBarComponent, BannerComponent, FooterComponent, GalleryComponent]
 })
 export class ProceduresLabComponent {
 
--- src/app/pages/procedures-sob/procedures-sob.component.ts
+++ src/app/pages/procedures-sob/procedures-sob.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from "../components/nav-bar/nav-bar.component";
 import { BannerComponent } from "../components/banner/banner.component";
 import { FooterComponent } from "../components/footer/footer.component";
+import { GalleryComponent } from "../components/gallery/gallery.component";
 
 @Component({
     selector: 'app-procedures-sob',
     standalone: true,
     templateUrl: './procedures-sob.component.html',
     styleUrl: './procedures-sob.component.css',
-    imports: [NavBarComponent, BannerComponent, FooterComponent]
+    imports: [NavBarComponent, BannerComponent, FooterComponent, GalleryComponent]
 })
 export class ProceduresSobComponent {
 

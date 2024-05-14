603bcec26cefa88b46f083552e287c3943ef8756
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2024-05-13 23:42:20 -0300

    gallery e maps

--- src/app/pages/page-awards/page-awards.component.ts
+++ src/app/pages/page-awards/page-awards.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from "../components/nav-bar/nav-bar.component";
 import { BannerComponent } from "../components/banner/banner.component";
 import { FooterComponent } from "../components/footer/footer.component";
+import { LocationComponent } from "../components/location/location.component";
+import { GalleryComponent } from "../components/gallery/gallery.component";
 
 @Component({
     selector: 'app-page-awards',
     standalone: true,
     templateUrl: './page-awards.component.html',
     styleUrl: './page-awards.component.css',
-    imports: [NavBarComponent, BannerComponent, FooterComponent]
+    imports: [NavBarComponent, BannerComponent, FooterComponent, LocationComponent, GalleryComponent]
 })
 export class PageAwardsComponent {
 
